3690ad6629ef00a3b182e1dc434ea2e62c274d41
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-04-12 11:40:47 -0400

    view controller add

---
+++ VideoApplication/VideoApplication/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  VideoApplication
+//
+//  Created by Canadore Student on 2023-04-12.
+//  Copyright © 2023 Canadore Student. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+
+}
