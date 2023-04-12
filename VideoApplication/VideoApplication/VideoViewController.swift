c6500a46428c23cbcfa2b350317a8512b21128a7
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-04-12 12:18:08 -0400

    add video

--- VideoApplication/VideoApplication/VideoViewController.swift
+++ VideoApplication/VideoApplication/VideoViewController.swift
@@ -25,7 +25,7 @@ class VideoViewController: UIViewController {
     }
     
     func startVideo(){
-        let url = URL(string: "")
+        let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726")
         player = AVPlayer(url: url!)
         avpController.player = player
         avpController.view.frame = myVideoView.bounds
